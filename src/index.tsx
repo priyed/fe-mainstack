2fde1a985afd3eedd66f606d27bc9d97f4e11f55
priyed <stanleyduye@gmail.com>
2023-11-24 13:14:51 +0100

    initialized project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOMClient.createRoot(document.getElementById("root")!);
+
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
